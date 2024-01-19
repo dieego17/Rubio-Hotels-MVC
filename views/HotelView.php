071468f109114cfb06afe6ddde224e82d61eb716
dieego17 <diegorubio1704.drs@gmail.com>
2024-01-19 11:21:55 +0100

    design when showing hotels done

--- views/HotelView.php
+++ views/HotelView.php
@@ -1,49 +1,52 @@
 <?php
 
     class HotelView{
-        public function mostrarHoteles($nuevosHoteles) {
-            
-            foreach ($nuevosHoteles as $hotel) {
-                
+        public function mostrarHoteles($nuevosHoteles) {               
 ?>
                 <div class="container__hoteles">
-                    <table>
-                        <thead>
-                            <th class="hotel__texto">FOTO</th>
-                            <th class="hotel__texto">NOMBRE</th>
-                            <th class="hotel__texto">DIRECCIÓN</th>
-                            <th class="hotel__texto">CIUDAD</th>
-                            <th class="hotel__texto">PAÍS</th>
-                            <th class="hotel__texto">NUM HABITACIONES</th>
-                            <th class="hotel__texto">DESCRIPCIÓN</th>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td class="hotel__texto"><?php echo $hotel->getFoto();?></td>
-                                <td class="hotel__texto"><?php echo $hotel->getNombre();?></td>
-                                <td class="hotel__texto"><?php echo $hotel->getDireccion();?></td>
-                                <td class="hotel__texto"><?php echo $hotel->getCiudad();?></td>
-                                <td class="hotel__texto"><?php echo $hotel->getPais();?></td>
-                                <td class="hotel__texto"><?php echo $hotel->getNum_habitaciones();?></td>
-                                <td class="hotel__texto"><?php echo $hotel->getDescripcion();?></td>
-                                <td class="hotel__texto"><a><a href="url" target="target">Ver detalles</a></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <!--<section class="section__hotel">
-                        <p class="hotel__texto"><?php echo $hotel->getNombre();?></p>
-                        <p class="hotel__texto"><?php echo $hotel->getDireccion();?></p>
-                        <p class="hotel__texto"><?php echo $hotel->getCiudad();?></p>
-                        <p class="hotel__texto"><?php echo $hotel->getPais();?></p>
-                        <p class="hotel__texto"><?php echo $hotel->getNum_habitaciones();?></p>
-                        <p class="hotel__texto"><?php echo $hotel->getDescripcion();?></p>
-                        <p class="hotel__texto"><?php echo $hotel->getFoto();?></p>
-                    </section>-->
+                    <main class="container__main">
+                        <section class="section__title">
+                            <h2 class="h2__titulo">NUESTROS HOTELES</h2>
+                            <a class="link__sesion" href="">CERRAR SESIÓN</a>
+                        </section>
+                        <section class="section__hotel">
+                            <table class="table__hoteles">
+                                <?php
+                                    foreach ($nuevosHoteles as $hotel) {
+                                        echo '<thead class="thead__hoteles">';
+                                            echo '<th class="th__hotel">FOTO</th>';
+                                            echo '<th class="th__hotel">NOMBRE</th>';
+                                            echo '<th class="th__hotel">DIRECCIÓN</th>';
+                                            echo '<th class="th__hotel">CIUDAD</th>';
+                                            echo '<th class="th__hotel">PAÍS</th>';
+                                            echo '<th class="th__hotel">NUM HABITACIONES</th>';
+                                            echo '<th class="th__hotel">DESCRIPCIÓN</th>';
+                                            echo '<th class="th__hotel"></th>';
+                                        echo '</thead>';
+                                        echo '<tbody>';
+                                            echo '<tr>';
+                                                echo '<td class="td__hotel">'.$hotel->getFoto().'</td>';
+                                                echo '<td class="td__hotel">'.$hotel->getNombre().'</td>';
+                                                echo '<td class="td__hotel">'.$hotel->getDireccion().'</td>';
+                                                echo '<td class="td__hotel">'.$hotel->getCiudad().'</td>';
+                                                echo '<td class="td__hotel">'.$hotel->getPais().'</td>';
+                                                echo '<td class="td__hotel">'.$hotel->getNum_habitaciones().'</td>';
+                                                echo '<td class="td__hotel">'.$hotel->getDescripcion().'</td>';
+                                                echo '<td class="td__hotel"><a class="td__link" href="url" target="target">Ver detalles</a></td>';
+                                            echo '</tr>';
+                                        echo '</tbody>';
+                                    }
+                                    
+                                ?>
+                            </table>
+                        </section>
+                    </main>
                 </div>
                 
 <?php
-            }
         }
     }
 ?>
+
+
 
